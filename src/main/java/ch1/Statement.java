e5c8c7cc7701530f515b6fef3797f996d68bcf45
hlmg <devhlmg@gmail.com>
2025-01-21 13:13:48 +0900

    파라미터 이름 변경 perf -> performance

--- src/main/java/ch1/Statement.java
+++ src/main/java/ch1/Statement.java
@@ -40,21 +40,21 @@ public class Statement {
 		return result;
 	}
 
-	private int amountFor(Performance perf, Play play) {
+	private int amountFor(Performance performance, Play play) {
 		var result = 0;
 		switch (play.type()) {
 			case "tragedy":
 				result = 40000;
-				if (perf.audience() > 30) {
-					result += 1000 * (perf.audience() - 30);
+				if (performance.audience() > 30) {
+					result += 1000 * (performance.audience() - 30);
 				}
 				break;
 			case "comedy":
 				result = 30000;
-				if (perf.audience() > 20) {
-					result += 10000 + 500 * (perf.audience() - 20);
+				if (performance.audience() > 20) {
+					result += 10000 + 500 * (performance.audience() - 20);
 				}
-				result += 300 * perf.audience();
+				result += 300 * performance.audience();
 				break;
 			default:
 				throw new IllegalArgumentException("알 수 없는 장르: " + play.type());
